d46bc17405d7b36512daea76a4737b286ff4b100
kommav <kommalapativ97@gmail.com>
2021-07-14 16:03:19 -0500

    App Fix #2

--- test.py
+++ test.py
@@ -88,7 +88,7 @@ def app_F():
     return iterations
 
 total = 0
-for x in range(app_F()):
+for x in range(app_F().result()):
     total += app_E(app_D(10, 7, app_C(app_A(), app_B()).result()))
     print(x)
 print(total)
