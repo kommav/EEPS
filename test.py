fab04394f30223a9b04aaf74936865870bb1c92b
kommav <kommalapativ97@gmail.com>
2021-07-23 09:14:21 -0500

    C/W = 2.00 Tests

--- test.py
+++ test.py
@@ -33,7 +33,7 @@ def fresh_config():
                 working_dir=working_dir,
                 storage_access=[FTPInTaskStaging(), HTTPInTaskStaging(), NoOpFileStaging()],
                 worker_debug=True,
-                cores_per_worker = 1.5,
+                cores_per_worker = 2,
                 max_workers = 3,
                 heartbeat_period=2,
                 heartbeat_threshold=5,
