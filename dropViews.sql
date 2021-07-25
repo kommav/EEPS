9b3bc5246c19e4c71e8223413ba183267e6fc27c
kommav <86332757+kommav@users.noreply.github.com>
2021-07-25 13:57:32 -0500

    dropViews.sql
    SQL File used for Drop Views for dbAnalysis.py

---
+++ dropViews.sql
@@ -0,0 +1,10 @@
+/* dropViews.sql - remove the specal sqlite3 "views" needed by wstat */
+d
+drop view runview;
+drop view nctaskview;
+drop view ndtaskview;
+drop view taskview;
+drop view sumv1;
+drop view sumv2;
+drop view summary;
+drop view plotstats;
