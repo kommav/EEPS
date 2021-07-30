78b7283780f89e4c2d0b2bd30242bd26261cfe21
AnanthHariharan <ananth.hariharan05@gmail.com>
2021-07-29 20:21:17 -0500

    2

--- cpw_eval.py
+++ cpw_eval.py
@@ -127,7 +127,7 @@ for i in range(len(cpw)):
     tEnd = time.perf_counter()
     totalTimes.append(tEnd - tStart)
     totalCost.append((tEnd-tStart)*(cores/cores_per_worker))
-    print (totalCost)
+    print(totalCost)
     tStart = tEnd
     print()
     print("Cores per worker: " + str(cores_per_worker))
