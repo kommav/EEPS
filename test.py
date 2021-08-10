4851feb5566528fbbba069ff71095df18f143e6b
AnanthHariharan <ananth.hariharan05@gmail.com>
2021-08-10 14:15:43 -0500

    New Applications

--- test.py
+++ test.py
@@ -51,7 +51,7 @@ def fresh_config():
                     channel=LocalChannel(),
                     init_blocks=0,
                     min_blocks=0,
-                    max_blocks=5,
+                    max_blocks=1,
                     launcher=SingleNodeLauncher(),
                 ),
             )
@@ -81,38 +81,158 @@ def app_A():
     return a
 
 @python_app
-def app_B():
+def app_B(x):
     time.sleep(4)
-    b = 2 + 2 / 2
+    b = x + 2 / 2
     return b
-
 
 @python_app
 def app_C(x, y):
     time.sleep(3)
     return x + y
 
-
 @python_app
-def app_D(x, y, z):
+def app_D(a,b,c):
     time.sleep(1)
-    return x * y // z
-
-
-@python_app
-def app_E(x):
-    time.sleep(5)
-    return x * x
+    d = a + b - c
+    a = d + b
+    return (a / d) + 2
 
 @python_app
-def app_F(x):
+def app_E(a,b,c,d):
+    time.sleep(7)
+    e = a + b - c / d
+    a = e * d + b
+    return (a / e) + 2
+
+@python_app
+def app_F(a,b,c,d,e):
+    time.sleep(3)
+    return (c - b) + e * (d / a)
+
+@python_app
+def app_G():
     time.sleep(9)
-    return 2*x
+    return 9 * 2
 
 @python_app
-def app_G(x):
+def app_H(x):
+    time.sleep(3)
+    return x / 2
+
+@python_app
+def app_I(x,y):
+    time.sleep(1)
+    z = x * x - (3 - 4 * y)
+    return (0 - z) + 2
+
+@python_app
+def app_J(x,y,z):
+    time.sleep(2)
+    return (x + y) * z / 2
+
+@python_app
+def app_K(a,b,c,d):
+    time.sleep(3)
+    e = a + b - c / d
+    a = e * d + b
+    return (a / e) + 2
+
+@python_app
+def app_L(a,b,c,d,e):
+    time.sleep(1)
+    return (c - b) + e * (d / a)
+
+@python_app
+def app_M():
+    time.sleep(4)
+    return 10 * 3 / 5
+
+@python_app
+def app_N(x):
+    time.sleep(3)
+    return x / 2
+
+@python_app
+def app_O(x,y):
+    time.sleep(6)
+    return x / 2 + y
+
+@python_app
+def app_P(x,y,z):
+    time.sleep(2)
+    return y - x + z * y / x
+
+@python_app
+def app_Q(a,b,c,d):
+    time.sleep(8)
+    p = a * a + b / (c - d)
+    return p * p / p + p
+
+@python_app
+def app_R(a,b,c,d,e):
+    time.sleep(2)
+    a = a + 9
+    b = a + b
+    c = b + c
+    d = c + d
+    e = d + e
+    r = e * a
+    return r
+
+@python_app
+def app_S(a,b,c,d,e,f):
+    time.sleep(4)
+    return a - f + b * c / d + e
+
+@python_app
+def app_T(a,b,c,d,e,f,g):
     time.sleep(3)
     return x/2
+
+@python_app
+def app_U(a,b,c,d,e,f,g,h):
+    time.sleep(6)
+    return x/2
+
+@python_app
+def app_V(a,b,c,d,e,f,g,h,i):
+    time.sleep(8)
+    a = a * b + 1
+    b = b * c + a
+    c = c * d + b
+    d = d * e + c
+    e = e * f + d
+    f = f / g + e
+    g = g / h + f
+    v = g + (h/f)
+    return v * 8 - i
+
+@python_app
+def app_W(a,b,c,d,e,f,g,h,i,j):
+    time.sleep(2)
+    w = j - a + e / (c * h)
+    return w / 3 + b * d - 2 + f / g * i
+
+@python_app
+def app_X(a,b,c,d,e,f,g,h,i,j,k):
+    time.sleep(3)
+    x = 6 * f - h / g + f
+    return x * 3 + (i * j - 4 * k / (a - b + c * d / e))
+
+@python_app
+def app_Y(a,b,c,d,e,f,g,h,i,j,k,l):
+    time.sleep(1)
+    a = a * (c + 8 / d * l)
+    b = e - a + 5 * k * (g - 7 + i * b + j / h)
+    y = b * 9 / f + 1
+    return 18 * y / 2
+
+@python_app
+def app_Z(a,b,c,d,e,f,g,h,i,j,k,l,m):
+    time.sleep(6)
+    z = (a + b) - c * d + e - f + (g - h) + i / j - (k + l)
+    return z * 3 / m + 1
 
 total = 0
 
